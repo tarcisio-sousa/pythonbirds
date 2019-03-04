5f85fca94d690b3758e5167ea98ad0cfb7140ecc
tarcisio <sousa.tarcisio.s@gmail.com>
2019-03-04 15:58:28 -0300

    Criados atributos de instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=29):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Tarcísio')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Sales'
+    print(p.nome)
+    print(p.idade)
